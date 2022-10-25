261b6d6fb136df56309657e27de7ca57d67dbd6f
glorycornelia <glorycornelia26@gmail.com>
2022-10-25 22:38:49 +0700

    Commit

--- order.php
+++ order.php
@@ -1,12 +1,14 @@
 <?php
 
+include 'connection.php';
+
 if(isset($_POST['submit'])) {
+    $id_plg = $_POST['id_plg'];
     $nama = $_POST['nama'];
     $alamat = $_POST['alamat'];
     $telpon = $_POST['telpon'];
 
-    echo "Nama  : " .$nama. "<br>";
-    echo "Alamat    : " .$alamat. "<br>";
-    echo "No Telpon    : " .$telpon. "<br>";
+    $sql = "INSERT INTO pelanggan (id_pelanggan, nama_pelanggan, alamat, no_telp) 
+            VALUES ('$id_plg', '$nama', '$alamat', '$telpon')";
 }
 ?>
