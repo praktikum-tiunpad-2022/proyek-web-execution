25425be32b843f0f95dff4ddd5ce705a5089a720
glorycornelia <glorycornelia26@gmail.com>
2022-12-06 18:43:57 +0700

    update database

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2022 at 07:35 AM
+-- Generation Time: Dec 06, 2022 at 12:42 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -38,8 +38,25 @@ CREATE TABLE `harga` (
 
 INSERT INTO `harga` (`tipe_harga`, `harga`) VALUES
 (1, 15000),
-(2, 20000),
-(3, 25000);
+(2, 16000),
+(3, 17000),
+(4, 18000),
+(5, 19000),
+(6, 20000),
+(7, 21000),
+(8, 22000),
+(9, 23000),
+(10, 24000),
+(11, 25000),
+(12, 26000),
+(13, 27000),
+(14, 28000),
+(15, 29000),
+(16, 30000),
+(17, 31000),
+(18, 32000),
+(19, 33000),
+(20, 34000);
 
 -- --------------------------------------------------------
 
@@ -49,7 +66,7 @@ INSERT INTO `harga` (`tipe_harga`, `harga`) VALUES
 
 CREATE TABLE `icecream` (
   `id_iceCream` int(3) NOT NULL,
-  `nama_iceCream` varchar(20) DEFAULT NULL,
+  `nama_iceCream` varchar(50) DEFAULT NULL,
   `topping` varchar(20) DEFAULT NULL,
   `tipe_harga` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -59,16 +76,16 @@ CREATE TABLE `icecream` (
 --
 
 INSERT INTO `icecream` (`id_iceCream`, `nama_iceCream`, `topping`, `tipe_harga`) VALUES
-(1, 'Chocolate', 'Oreo', 1),
-(2, 'Vanilla', 'Caramel', 1),
-(5, 'Chocochips Ice Cream', 'Kacang', 28000),
-(6, 'Snow Oreo Ice Cream', 'Oreo', 17000),
-(7, 'Choco-Mocca Ice Crea', 'Cokelat', 21000),
-(8, 'Sweet Corn Ice Cream', 'Kacang', 25000),
-(9, 'Sweet Vanilla Ice Cr', 'Kismis', 32000),
-(10, 'Biscuit Ice Cream', 'Cokelat', 18000),
-(11, 'Wealth Chocolate Ice', 'Cokelat', 27000),
-(12, 'Sweet Cutton Ice Cre', 'Susu', 15000);
+(1, 'Chocolate Blue', 'Oreo', 2),
+(5, 'Chocochips Ice Cream', 'Kacang', 14),
+(6, 'Snow Oreo Ice Cream', 'Oreo', 3),
+(7, 'Choco-Mocca Ice Cream', 'Cokelat', 7),
+(8, 'Sweet Corn Ice Cream', 'Kacang', 11),
+(9, 'Sweet Vanilla Ice Cream', 'Kismis', 18),
+(10, 'Biscuit Ice Cream', 'Cokelat', 4),
+(11, 'Wealth Chocolate Ice Cream', 'Cokelat', 13),
+(12, 'Sweet Cutton Ice Cream', 'Susu', 1),
+(20, 'Santa Oreo Ice cream', 'Kacang', 14);
 
 -- --------------------------------------------------------
 
@@ -80,39 +97,34 @@ CREATE TABLE `pelanggan` (
   `id_pelanggan` int(3) NOT NULL,
   `nama_pelanggan` varchar(30) DEFAULT NULL,
   `alamat` text DEFAULT NULL,
-  `no_telp` int(12) DEFAULT NULL
+  `no_telp` int(12) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pelanggan`
 --
 
-INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat`, `no_telp`) VALUES
-(1, 'Andi', 'Bandung', 162099000),
-(2, 'Budi', 'Jakarta', 18246098),
-(3, 'Candra', 'Padang', 134665270),
-(4, 'Dina', 'Semarang', 146864653),
-(5, 'Emil', 'Lampung', 130182291),
-(6, 'Lim', 'Bandung', 112376324),
-(7, 'Vina', 'Surabaya', 2147483647),
-(8, 'Rika', 'Bandung', 231435643),
-(9, 'Aen', 'Lampung', 2147483647),
-(10, 'Kenneth', 'Surakarta', 2147483647),
-(11, 'Serena', 'Aceh', 493827267),
-(12, 'Serena', 'Aceh', 493827267),
-(13, 'Sisca', 'Bandung', 839462787),
-(14, 'Nicho', 'Yogyakarta', 2147483647),
-(15, 'Mika', 'Yogyakarta', 2147483647),
-(16, 'Villenn', 'Jakarta', 34456322),
-(17, 'Eilen', 'Sumatra Barat', 24243251),
-(18, 'Elsa', 'Bandung', 12433),
-(19, NULL, NULL, NULL),
-(20, 'qwe', 'sdad', 1222),
-(21, NULL, NULL, NULL),
-(22, NULL, NULL, NULL),
-(23, NULL, NULL, NULL),
-(24, NULL, NULL, NULL),
-(25, NULL, NULL, NULL);
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat`, `no_telp`, `email`) VALUES
+(9, 'Aen', 'Lampung', 2147483647, 'Aen@gmail.com'),
+(10, 'Kenneth', 'Surakarta', 2147483647, ''),
+(11, 'Serena', 'Aceh', 493827267, ''),
+(12, 'Serena', 'Aceh', 493827267, ''),
+(13, 'Sisca', 'Bandung', 839462787, ''),
+(14, 'Nicho', 'Yogyakarta', 2147483647, ''),
+(15, 'Mika', 'Yogyakarta', 2147483647, ''),
+(16, 'Villenn', 'Jakarta', 34456322, ''),
+(17, 'Eilen', 'Sumatra Barat', 24243251, ''),
+(18, 'Elsa', 'Bandung', 12433, ''),
+(20, 'qwe', 'sdad', 1222, ''),
+(26, 'Damar', 'Surabaya', 1234321567, ''),
+(29, 'Lim', 'Yogyakarta', 121356743, ''),
+(31, 'Violet', 'Surakarta', 346573823, 'violetX@gmail.com'),
+(32, 'Rika', 'Yogyakarta', 493827267, 'rika@gmail.com'),
+(33, 'Miya', 'Bandung', 2147483647, 'miya339@gmail.com'),
+(34, 'sannie', 'Palangkaraya', 214748340, 'sanny@gmail.com'),
+(35, 'Vina', 'Yogyakarta', 202742661, 'viina23@gmail.com'),
+(36, 'Glory', 'Jatinangor', 182637493, 'glorycornelia26@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -131,9 +143,16 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`no_transaksi`, `id_pelanggan`, `id_iceCream`) VALUES
-(1, 1, 2),
-(2, 2, 3),
-(3, 3, 4);
+(7, 29, 9),
+(8, 13, 10),
+(9, 35, 5),
+(10, 35, 8),
+(11, 35, 11),
+(12, 35, 12),
+(13, 34, 7),
+(14, 35, 20),
+(15, 34, 11),
+(16, 36, 9);
 
 -- --------------------------------------------------------
 
@@ -144,15 +163,23 @@ INSERT INTO `transaksi` (`no_transaksi`, `id_pelanggan`, `id_iceCream`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `level` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`) VALUES
-(2, 'diona12@gmail.com', 'diona12345');
+INSERT INTO `users` (`id`, `email`, `password`, `level`) VALUES
+(1, 'anemonlaut7@gmail.com', '$2y$10$QAqNeF4e1CLpdXLIQbpvfOTcox37X/JdP3JWqD7tlcet5VNXn5eRa', 1),
+(2, 'Ellen12@gmail.com', '$2y$10$pUI4SqaCDA2yFqFgMfo9IuV1JU5kwAEY7DXqy3QhAZRDPmBZdfhOO', 2),
+(3, 'limo31@gmail.com', '$2y$10$l2lD9VXVTDEbm79R/fkPnO5K8UjB0B/wROjc7EZiAiY7nI2Kb0qt2', 1),
+(4, 'rika@gmail.com', '$2y$10$SeeeBe9Zq/0enh1U7zF7PesVQBBqBPUj1criaJoVOKuwzeI79IFAm', 2),
+(5, 'testing@gmail.com', '$2y$10$6/ZXmF4fAK58Y3K/o6EUJukYsGWOaf/F.FDXR9pDHd.Sbaoc1xgUC', 1),
+(6, 'sanny@gmail.com', '$2y$10$qVxutZHD36Kx/am/UdcaPuUjPfQbGCQ0pPdU/bQWhifR2QjunDWFa', 2),
+(7, 'viina23@gmail.com', '$2y$10$euQo.5h7Ki2XFubjDVi4q.FG05KRr2cE.UVHuOSeAxlexnPloHkBW', 1),
+(8, 'glorycornelia26@gmail.com', '$2y$10$/wmK071LwwI1QIVZCN2t5OSNK9XnnPJae.jBaW4h4oNsHml6BvFKe', 1);
 
 --
 -- Indexes for dumped tables
@@ -175,7 +202,8 @@ ALTER TABLE `icecream`
 -- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  ADD PRIMARY KEY (`id_pelanggan`);
+  ADD PRIMARY KEY (`id_pelanggan`),
+  ADD KEY `idx_email` (`email`);
 
 --
 -- Indexes for table `transaksi`
@@ -199,19 +227,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_pelanggan` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `no_transaksi` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `no_transaksi` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
